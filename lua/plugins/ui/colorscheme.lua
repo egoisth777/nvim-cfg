7fc3541b45dbeb826f6e774078c2b3011c755fd4
Yueyang Li <egoistheresy@outlook.com>
2026-04-02 13:34:09 -0700

    auto-sync

--- lua/plugins/ui/colorscheme.lua
+++ lua/plugins/ui/colorscheme.lua
@@ -7,7 +7,6 @@
     3. cyberdream
     5. nightfox
 --]]
-
 -- END- - - - - - - - - - - - - - - - - - - - - -
 
 return
@@ -22,7 +21,7 @@ return
             transparent_background = true, -- Enables full transparency for backgrounds
         },
     },
-
+    
     -- abyss colorscheme
     {
         "barrientosvctor/abyss.nvim",
@@ -55,74 +54,12 @@ return
                 transparent = true,
                 italic_comments = true,
                 borderless_telescope = true, -- This affects fzf-lua too
-                borderless_pickers = true,
-                terminal_colors = false,
+                borderless_pickers = true, -- 
+                terminal_colors = true,
+                hide_fillchars = true,
                 cache = false,
                 -- Add theme customizations for specific syntax elements
                 highlights = {
-
-                    -- Global Settings----------------------------------------------------------
-                    -- White
-                    Identifier = { fg = "#ffffff", bold = true },
-
-                    -- Yellow (Face Value)
-
-                    Number = { fg = "#f1ff5e", italic = true },
-                    String = { fg = "#f1ff5e", italic = true },
-
-                    -- Your current subtle green
-                    Comment = { fg = "#77aa77", bold = false, italic = true },
-
-                    -- Orange
-                    Keyword = { fg = "#ffbd5e", bold = false },
-
-                    -- Magenta
-                    Type = { fg = "#ff5ef1", bold = false },
-
-                    -- Neon Green
-                    Function = { fg = "#5eff6c", bold = true, italic = true },
-                    PreProc = { fg = "#5eff6c", bold = true, italic = true },
-                    Operator = { fg = "#5eff6c" },
-
-                    -- Neon Pink
-                    Special = { fg = "#ff5ea0", italic = true },
-
-                    -- Blue
-                    Delimiter = { fg = "#5ea1ff", bold = true },
-
-
-                    -- END OF GLOBAL SETTINGS ---------------------------------------------------
-
-                    -- LSP and Language Settings
-
-                    -- General Language Settings
-                    ["@variable"] = { fg = "#ffffff", bold = true },
-                    ["@keyword.type"] = { link = "Keyword" },
-                    ["@constructor"] = { link = "Function" },
-                    ["@type.builtin"] = { link = "Type" },
-                    ["@module"] = { fg = "#5ef1ff" },
-
-                    -- cpp settings
-                    ["@keyword.conditional.ternary.cpp"] = { link = "Operator" },
-                    ["@lsp.mod.constructorOrDestructor.cpp"] = { link = "Function" },
-                    ["@lsp.typemod.class.constructorOrDestructor.cpp"] = { link = "Function" },
-
-
-                    -- Cursor related ----------------------------------------------------------
-                    
-
-                    CursorLine           = { bg = "#354456", bold = true },
-                    Visual               = { bg = "#507789", italic = true, bold = true, undercurl = true },
-
-                    -- Word under cursor (via LSP/illuminate)
-                    LspReferenceText     = { bg = "#507789" },
-                    LspReferenceRead     = { bg = "#507789" },
-                    LspReferenceWrite    = { bg = "#507789" },
-
-                    IlluminatedWordText  = { bg = "#507789" },
-                    IlluminatedWordRead  = { bg = "#507789" },
-                    IlluminatedWordWrite = { bg = "#507789" },
-
                     -- Settings for snacks.nvim
                     -- Make the main picker window (explorer) transparent
                     SnacksPickerNormal = { bg = "NONE" },
